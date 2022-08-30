90c80e3ae6af4d58508083f2e41a1795e19627c8
Nichevithan Jegatheeswaran <55763640+blurredvision@users.noreply.github.com>
2022-08-30 21:55:57 +0100

    Fixed some more Errors

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {AiOutlineMenu, AiOutlineClose, AiOutlineMail} from 'react-icons/ai'
 import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import Link from 'next/link'
 
 function Main() {
   return (
@@ -9,38 +10,38 @@ function Main() {
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
                 <p className='uppercase text-sm tracking-widest text-gray-600'>
-                    LET'S BUILD SOMETHING TOGETHER
+                    LET&apos;S BUILD SOMETHING TOGETHER
                 </p>
                 <h1 className='py-4 text-gray-700'>
-                    Hi, I'm <span className='text-[#5651e5]'>Niche</span> 
+                    Hi, I&apos;m <span className='text-[#5651e5]'>Niche</span> 
                 </h1>
                 <h1 className='py-4 text-gray-700'>
                     A Front-End Web Developer
                 </h1>
                 <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                    I'm a Front-End developer specialised in building responsive front-end web applications. Currently, I'm exploring NextJS and the potential it has whilst learning back-end technologies. 
+                    I&apos;m a Front-End developer specialised in building responsive front-end web applications. Currently, I&apos;m exploring NextJS and the potential it has whilst learning back-end technologies. 
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                    <a href='https://www.linkedin.com/in/nichej'>
+                    <Link href='https://www.linkedin.com/in/nichej'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursour-pointer hover:scale-105 ease-in duration-300'>
                                 <FaLinkedinIn />
                         </div>
-                    </a>
-                    <a href='https://github.com/blurredvision'>
+                    </Link>
+                    <Link href='https://github.com/blurredvision'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursour-pointer hover:scale-105 ease-in duration-300'>
                             <FaGithub />
                         </div>
-                    </a>
-                    <a href='/#about'>
+                    </Link>
+                    <Link href='/#about'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursour-pointer hover:scale-105 ease-in duration-300'>
                             <BsFillPersonLinesFill />
                         </div>
-                    </a>
-                    <a href='/#contact'>
+                    </Link>
+                    <Link href='/#contact'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursour-pointer hover:scale-105 ease-in duration-300'>
                             <AiOutlineMail />
                         </div>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -89,7 +89,7 @@ function Navbar() {
               </div>
             </div>
             <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4'>Turning dreams into reality togheter</p>
+              <p className='w-[85%] md:w-[90%] py-4'>Turning dreams into reality together</p>
             </div>
           </div>
           <div className='py-4 flex-col'>
@@ -111,7 +111,7 @@ function Navbar() {
               </Link>
             </ul>
             <div className='pt-40'>
-              <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+              <p className='uppercase tracking-widest text-[#5651e5]'>Let&apos;s Connect</p>
               <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursour-pointer hover:scale-105 ease-in duration-300'>
                   <FaLinkedinIn />
