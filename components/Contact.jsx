ad2fc1eddcf73c58aa78d1914081fc4ac75b400c
Nichevithan Jegatheeswaran <55763640+blurredvision@users.noreply.github.com>
2022-10-11 14:20:39 +0200

    Added contact functionality

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -53,28 +53,28 @@ function Contact() {
 
                 <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
                     <div className='p-4'>
-                        <form>
+                    <form action="https://formsubmit.co/f7ca30b4ea85d823348e59de1c9d8337" method="POST">
                             <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                                 <div className='flex flex-col'>
                                     <label className='uppercase text-sm py-2'>Name</label>
-                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'/>
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='name' required/>
                                 </div>
                                 <div className='flex flex-col'>
                                     <label className='uppercase text-sm py-2'>Phone Number</label>
-                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'/>
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='phone number'/>
                                 </div>
                             </div>
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-sm py-2'>Email</label>
-                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='email'/>
+                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='email' name='email' required/>
                             </div>
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-sm py-2'>Subject</label>
-                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'/>
+                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='subject' required/>
                             </div>
                             <div className='flex flex-col py-2'>
                                 <label className='uppercase text-sm py-2'>Message</label>
-                                <textarea className='border-2 rounded-lg p-3 border-gray-300' rows={10}></textarea>
+                                <textarea className='border-2 rounded-lg p-3 border-gray-300' rows={10} name='message' required></textarea>
                             </div>
                             <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
                         </form>
